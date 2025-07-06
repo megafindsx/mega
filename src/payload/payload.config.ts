79d9f59486727009d899c7d343c57ae9f21cc18f
megafindsx <123naveena123@gmail.com>
2025-07-06 21:03:18 +0530

    payload.config.ts

--- src/payload/payload.config.ts
+++ src/payload/payload.config.ts
@@ -1,153 +1,31 @@
 import dotenv from "dotenv";
-dotenv.config();
-
-import { webpackBundler } from "@payloadcms/bundler-webpack"; // bundler-import
-import { mongooseAdapter } from "@payloadcms/db-mongodb"; // database-adapter-import
-import { payloadCloud } from "@payloadcms/plugin-cloud";
-// import formBuilder from '@payloadcms/plugin-form-builder'
-import nestedDocs from "@payloadcms/plugin-nested-docs";
-import redirects from "@payloadcms/plugin-redirects";
-import seo from "@payloadcms/plugin-seo";
-import type { GenerateTitle } from "@payloadcms/plugin-seo/types";
-import stripePlugin from "@payloadcms/plugin-stripe";
-import { slateEditor } from "@payloadcms/richtext-slate"; // editor-import
-
 import path from "path";
+
 import { buildConfig } from "payload/config";
+import type { CollectionConfig } from "payload/types";
 
 import Categories from "./collections/Categories";
-import { Media } from "./collections/Media";
-import { Orders } from "./collections/Orders";
-import { Pages } from "./collections/Pages";
+import Media from "./collections/Media";
+import Orders from "./collections/Orders";
+import Pages from "./collections/Pages";
 import Products from "./collections/Products";
+import Redirects from "./collections/Redirects";
 import Users from "./collections/Users";
-import BeforeDashboard from "./components/BeforeDashboard";
-import BeforeLogin from "./components/BeforeLogin";
-import { createPaymentIntent } from "./endpoints/create-payment-intent";
-import { customersProxy } from "./endpoints/customers";
-import { productsProxy } from "./endpoints/products";
-import { seed } from "./endpoints/seed";
-import { Footer } from "./globals/Footer";
-import { Header } from "./globals/Header";
-import { Settings } from "./globals/Settings";
-import { priceUpdated } from "./stripe/webhooks/priceUpdated";
-import { productUpdated } from "./stripe/webhooks/productUpdated";
-
-const generateTitle: GenerateTitle = () => {
-  return "My Store";
-};
-
-const mockModulePath = path.resolve(__dirname, "./emptyModuleMock.js");
+import { CustomEndpoint } from "./endpoints/custom-endpoint";
+import { megafindsPlugin } from "./plugins/megafindsPlugin";
+import { stripePlugin } from "./plugins/stripe";
 
 dotenv.config({
   path: path.resolve(__dirname, "../../.env"),
 });
 
 export default buildConfig({
-  admin: {
-    user: Users.slug,
-    bundler: webpackBundler(), // bundler-config
-    components: {
-      beforeLogin: [BeforeLogin],
-      beforeDashboard: [BeforeDashboard],
-    },
-    webpack: (config) => {
-      return {
-        ...config,
-        resolve: {
-          ...config.resolve,
-          alias: {
-            ...config.resolve?.alias,
-            dotenv: path.resolve(__dirname, "./dotenv.js"),
-            [path.resolve(
-              __dirname,
-              "collections/Products/hooks/beforeChange"
-            )]: mockModulePath,
-            [path.resolve(
-              __dirname,
-              "collections/Users/hooks/createStripeCustomer"
-            )]: mockModulePath,
-            [path.resolve(__dirname, "collections/Users/endpoints/customer")]:
-              mockModulePath,
-            [path.resolve(__dirname, "endpoints/create-payment-intent")]:
-              mockModulePath,
-            [path.resolve(__dirname, "endpoints/customers")]: mockModulePath,
-            [path.resolve(__dirname, "endpoints/products")]: mockModulePath,
-            [path.resolve(__dirname, "endpoints/seed")]: mockModulePath,
-            stripe: mockModulePath,
-            express: mockModulePath,
-          },
-        },
-      };
-    },
-  },
-  editor: slateEditor({}), // editor-config
-  db: mongooseAdapter({
-    url: process.env.MONGODB_URI, // ✅ Fixed here
-  }),
   serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
-  collections: [Pages, Products, Orders, Media, Categories, Users],
-  globals: [Settings, Header, Footer],
-  typescript: {
-    outputFile: path.resolve(__dirname, "payload-types.ts"),
+  admin: {
+    user: "users",
   },
-  graphQL: {
-    schemaOutputFile: path.resolve(__dirname, "generated-schema.graphql"),
-  },
-  cors: [
-    "https://checkout.stripe.com",
-    process.env.PAYLOAD_PUBLIC_SERVER_URL || "",
-  ].filter(Boolean),
-  csrf: [
-    "https://checkout.stripe.com",
-    process.env.PAYLOAD_PUBLIC_SERVER_URL || "",
-  ].filter(Boolean),
-  endpoints: [
-    {
-      path: "/create-payment-intent",
-      method: "post",
-      handler: createPaymentIntent,
-    },
-    {
-      path: "/stripe/customers",
-      method: "get",
-      handler: customersProxy,
-    },
-    {
-      path: "/stripe/products",
-      method: "get",
-      handler: productsProxy,
-    },
-    {
-      path: "/seed",
-      method: "get",
-      handler: seed,
-    },
-  ],
-  plugins: [
-    // formBuilder({}),
-    stripePlugin({
-      stripeSecretKey: process.env.STRIPE_SECRET_KEY || "",
-      isTestKey: Boolean(process.env.PAYLOAD_PUBLIC_STRIPE_IS_TEST_KEY),
-      stripeWebhooksEndpointSecret: process.env.STRIPE_WEBHOOKS_SIGNING_SECRET,
-      rest: false,
-      webhooks: {
-        "product.created": productUpdated,
-        "product.updated": productUpdated,
-        "price.updated": priceUpdated,
-      },
-    }),
-    redirects({
-      collections: ["pages", "products"],
-    }),
-    nestedDocs({
-      collections: ["categories"],
-    }),
-    seo({
-      collections: ["pages", "products"],
-      generateTitle,
-      uploadsCollection: "media",
-    }),
-    payloadCloud(),
-  ],
+  collections: [Users, Media, Categories, Products, Orders, Pages, Redirects],
+  globals: [],
+  endpoints: [CustomEndpoint],
+  plugins: [stripePlugin(), megafindsPlugin()],
 });
